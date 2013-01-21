462172d947781fc29b443d4b647ee16395b92f9b
Robert Chipperfield <robert@syxis.co.uk>
2013-01-21 23:00:53 +0000

    Make a new row automatically when you start typing the callsign of the previous row

---
+++ ADIFLogEntry/Scripts/adiflogentry.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+
+    var addQsoEditRow = function () {
+        var qsoEntryRow = $("#qsoEntryRow");
+        var newRow = qsoEntryRow.clone(false);
+
+        qsoEntryRow.attr("id", null);
+        qsoEntryRow.addClass("unsavedQsoRow");
+        qsoEntryRow.find(".qsocallsign").off("keypress.makenewrow");
+
+        $("#qsotable tbody").append(newRow);
+        newRow.attr("id", "qsoEntryRow");
+        var newcallsignbox = newRow.find(".qsocallsign");
+        newcallsignbox.on("keypress.makenewrow", addQsoEditRow);
+
+    };
+    $(".qsocallsign").on("keypress.makenewrow", addQsoEditRow);
+
+});
